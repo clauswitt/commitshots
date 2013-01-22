fe958c4ed96058afde974f81ee6500a76c3b8dff
Claus Witt <claus@wittnezz.dk>
2013-01-22 17:48:34 +0100

    initial implementation

---
+++ lib/commits.rb
@@ -0,0 +1,12 @@
+def each_commit()
+  commit_ids = `git rev-list --all --pretty=oneline`.split("\n")
+  commit_ids.reverse!
+
+  commit_ids.each do |line|
+    parts = line.split(' ')
+    id = parts[0]
+    msg = line.gsub(id, '')
+    `git checkout #{id}`
+    yield id, msg
+  end
+end
